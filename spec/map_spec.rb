5f8bdf65833068240a5e0b81d614d7e99ad73ae7
Jan Kadijk <j.kadijk@gmail.com>
2009-11-28 23:15:43 +0100

    Added some tests

--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -14,14 +14,33 @@ describe "GoogleMap::Map" do
     map.should respond_to 'to_html'
   end
   
+  it "should be able to add markers" do
+    map = GoogleMap::Map.new('map')
+    map.markers.empty?.should be true
+    map.markers << GoogleMap::Marker.new([10.5,5.6], :title => 'Title', :icon => 'icon.png')
+    map.markers.length.should be 1
+  end
+  
 end
 
 describe "Marker" do
+  
+  before(:each) do
+   @marker = GoogleMap::Marker.new([10.5,5.6], :title => 'Title', :icon => 'icon.png')
+  end
+  
   it "should initialize options" do
-    marker = GoogleMap::Marker.new([10.5,5.6], :title => 'Title', :icon => 'icon.png')
-    marker.position.should == [10.5,5.6]
-    marker.title.should == 'Title'
-    marker.icon.should == 'icon.png'
+    @marker.position.should == [10.5,5.6]
+    @marker.title.should == 'Title'
+    @marker.icon.should == 'icon.png'
+  end
+  
+  it "should respond to to_js" do
+    @marker.should respond_to 'to_js'
+  end
+  
+  it "should output js to create marker" do
+    @marker.to_js.should include "marker = new google.maps.Marker"
   end
   
 end
